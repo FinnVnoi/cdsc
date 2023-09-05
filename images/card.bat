da2ba0531fe3e65392ef9687c810747fb054ea3c
FinnVnoi <nguyen0971539604@gmail.com>
2023-09-05 15:59:50 +0700

    BETA

--- images/card.bat
+++ images/card.bat
@@ -1,12 +1,20 @@
 @echo off
 setlocal enabledelayedexpansion
-set "prefix=card"
-set "extension=.jpg"
-set "counter=1"
 
-for %%f in (*.jpeg) do (
-    ren "%%f" "!prefix!!counter!!extension!"
+set sourceDir=C:\Users\Administrator\Documents\GitHub\chess\images
+set outputDir=C:\Users\Administrator\Documents\GitHub\cdsc\images
+
+if not exist "%outputDir%" mkdir "%outputDir%"
+
+set counter=1
+for %%f in ("%sourceDir%\*") do (
+    set "fileName=%%~nxf"
+    set /a "newCounter=counter+1"
+    set /a "newCounter=newCounter*2-1"
+    copy "%%f" "%outputDir%\card!newCounter!.jpg"
+    set /a "newCounter+=1"
+    copy "%%f" "%outputDir%\card!newCounter!.jpg"
     set /a "counter+=1"
 )
 
-echo Đổi tên thành công!
+endlocal
